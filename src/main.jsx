429d5e3c9abff23d34277df859911890854794d2
Basil Goodluck <basilgoodluck22@gmail.com>
2024-05-07 22:21:23 +0100

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client"
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      }
+    ]
   },
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,17 @@
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom"
+import { getContacts, createContact } from "../contacts"
+
+export async function action() {
+    const contact = await createContact();
+    return contact ;
+}
+
+export async function loader() {
+    const contacts = await getContacts();
+    return contacts
+}
 export default function Root () {
+    const contacts = useLoaderData();
     return (
         <>
             <div id="sidebar">
@@ -21,20 +34,38 @@ export default function Root () {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Basil Goodluck</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Basil Desmond</a>
-                        </li>
-                    </ul>
+                    {contacts ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No Contacts</i>
+                        </p>
+                    )} 
+                
                 </nav>
+            </div>
+            <div id="detail">
+                <Outlet />
             </div>
         </>
     )
